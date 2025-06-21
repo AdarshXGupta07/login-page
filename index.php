00c560e5c6222d88145b8743b87e729aec4f3736
Adarsh Gupta <adarxxxshgupta@gmail.com>
2025-06-21 21:11:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+Can't create, edit, or upload … If your storage is full for 2+ years, your files may be deleted from Drive and Gmail. Get 30 GB of storage for ₹59.00 ₹15.00 for 1 month.
+<?php  if (count($errors) > 0) : ?>
+  <div class="error">
+  	<?php foreach ($errors as $error) : ?>
+  	  <p><?php echo $error ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
